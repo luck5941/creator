6099ac44b97ce72fc800f86d020cb52f88bbdd9b
dcamarmas <diego.camarmas.alonso@gmail.com>
2019-09-20 21:00:15 +0200

    1.3-cm: initial version

--- js/creator.sh
+++ js/creator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-   var creator  = require('./min.creator_node.js') ;
-   var fs       = require('fs') ;
+   var fs      = require('fs') ;
+   var creator = require('./min.creator_node.js') ;
 
 
    //
@@ -11,7 +11,7 @@
    if (process.argv.length < 3)
    {
        console.log("") ;
-       console.log("Usage: creator.sh <assembly file>.s") ;
+       console.log("Usage: creator.sh <architecture> <assembly file>.s") ;
        console.log("") ;
 
        return 0 ;
@@ -24,8 +24,12 @@
  
    try 
    {
-       var assembly = fs.readFileSync(process.argv[1], 'utf8') ;
-       var result   = creator.assembly_compile(assembly) ;
+       var architecture = fs.readFileSync(process.argv[2], 'utf8') ;
+       var result       = creator.load_architecture(architecture) ;
+       console.log(result) ;
+
+       var assembly     = fs.readFileSync(process.argv[3], 'utf8') ;
+       var result       = creator.assembly_compile(assembly) ;       
        console.log(result) ;
    }
    catch (e)
